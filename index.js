462ca42077f8202c5164089300e90fe3109f9ea6
Bene_Tesla <78994881+benetesla@users.noreply.github.com>
2023-01-20 13:45:16 -0300

    EJS START TEMPLATE

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const app = express();
+const ejs = require('ejs');
+const path = require('path');
 const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-//temple    
+
+//templates  
+app.set("view engine", "ejs");
+//routes
+app.set("views", path.join(__dirname, "view"));
 app.get('/', (req, res) => {
    res.render('index');
 });
